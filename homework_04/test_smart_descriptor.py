ddd279cc10c6cc5da7b5d66c376638d044d28248
Anatoly Makarevich <anatoly_mak@yahoo.com>
2018-12-16 02:57:23 +0300

    Add actual unit tests to test. :)

--- homework_04/test_smart_descriptor.py
+++ homework_04/test_smart_descriptor.py
@@ -1,4 +1,5 @@
 
+import unittest
 import os 
 from smart_descriptor import smart_property 
 
@@ -73,38 +74,80 @@ class A(object):
         pass
 
 
-a = A()
-a.p
-a.q
-a.r
-
-# NOTE: these are only generated upon access at least one
-# I couldn't figure out how to get the parent from __new__ or __init__ :/
-a._p
-a._q
-a._r
-
-#
-a.q = 101
-a.q = '99.9'
-
-try:
-    a.r = 4
-except AttributeError:
-    print("All good!")
-
-
 class B(object):
     """Another example class."""
 
 
-try:
-    a.p2
-except ValueError as e:
-    print("All good: %s" % e)
+class Test_smart_property(unittest.TestCase):
+    """Tests the smart_property.
+    
+    Honestly, these are pretty basic test cases.
+    We'd """
 
-print(
-    "Path exists (%s): %s",
-    os.path.exists(a.p3),
-    a.p3
-)
+    def setUp(self):
+        dcs = './docs'
+        if not os.path.exists(dcs):
+            os.mkdir(dcs)  # for a.p
+
+        self.a = A()
+        self.b = B()
+
+    def tearDown(self):
+        os.rmdir('./docs')  # for a.p
+
+        pt = self.a.p3
+        # NOTE: no race condition during test, so whatever
+        if os.path.exists(pt):
+            os.remove(pt)
+        
+    def test_p(self):
+        """Tests A.p, p1, p2 properties."""
+
+        a = self.a
+
+        with self.assertRaises(AttributeError):
+            a._p  # because we didn't ever access a.p
+        self.assertEqual(a.p, os.path.abspath('./docs'))
+        a._p
+
+        a.p = './pics'
+
+        with self.assertRaises(ValueError):
+            a.p2  # because the path doesn't exist!
+
+        # This will automatically make the path
+        self.assertTrue(os.path.exists(a.p3))
+        # NOTE: it's later killed by self.tearDown()
+
+    def test_r(self):
+        """Tests A.r property (readonly)"""
+
+        a = self.a        
+
+        a.r
+
+        with self.assertRaises(AttributeError):
+            a.r = 4  # because readonly
+
+        # Still allowed to cheat
+        a._r = 4
+        self.assertEqual(a.r, 4)
+
+    def test_q(self):
+        """Tests A.q property (float, mandatory)"""
+
+        a = self.a  
+
+        # Accept any of the following formats
+        a.q = 101
+        a.q = '99.9'
+        self.assertAlmostEqual(a.q, 99.9, places=2)
+        
+        a.q = '0xBEEF'
+
+        with self.assertRaises(ValueError):
+            a.q = 'BEEF'  # not a valid hex literal
+
+
+if __name__ == "__main__":
+    unittest.main()
